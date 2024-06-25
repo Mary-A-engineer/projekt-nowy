691b79c6ed198122727e859cfedbdf6d913f29de
Mary-A-engineer <Mary.A.engineer@outlook.com>
2024-06-25 19:03:59 +0200

    Task1: Added argument parsing

---
+++ task1.py
@@ -0,0 +1,10 @@
+import argparse
+import json
+import yaml
+import xml.etree.ElementTree as Etree
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Narzędzie do konwersji danych')
+    parser.add_argument('input_file', type=str, help='Ścieżka do pliku wejściowego')
+    parser.add_argument('output_file', type=str, help='Ścieżka do pliku wyjściowego')
+    return parser.parse_args()
