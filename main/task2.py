045222acdff870225a45bba0fe7ca325a8b038b5
Mary-A-engineer <Mary.A.engineer@outlook.com>
2024-06-25 19:48:27 +0200

    Added data loading functions

---
+++ main/task2.py
@@ -0,0 +1,8 @@
+def load_json(file_path):  #task2
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+    except json.JSONDecodeError as e:
+        print(f"Błąd podczas odczytu pliku JSON: {e}")
+        return None
